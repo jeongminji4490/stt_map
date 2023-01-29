e1bae56a4916f59430a7d9ab9a294c81e0168371
jeongminji4490 <hamins97@naver.com>
2023-01-29 16:08:38 +0900

    add google maps

--- app/src/main/java/com/example/miniproject1/MainActivity.kt
+++ app/src/main/java/com/example/miniproject1/MainActivity.kt
@@ -12,54 +12,115 @@ import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.firstttsproject.R
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private var speechRecognizer: SpeechRecognizer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestPermission()
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         setContent {
-            StartSpeechButton()
+            Main()
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun StartSpeechButton() {
-        var space by rememberSaveable { mutableStateOf("") }
+    fun Main() {
+        var text by rememberSaveable { mutableStateOf("") }
+        val seoul = LatLng(37.532600, 127.024612)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(seoul, 10f)
+        }
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(
-                onClick = { startSpeech() },
+                onClick = {
+                    val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+                    speechRecognizer?.startListening(speechRecognizerIntent)
+
+                    speechRecognizer?.setRecognitionListener(object : RecognitionListener {
+                        override fun onReadyForSpeech(bundle: Bundle) {}
+                        override fun onBeginningOfSpeech() {
+                        }
+
+                        override fun onRmsChanged(v: Float) {}
+                        override fun onBufferReceived(bytes: ByteArray) {}
+                        override fun onEndOfSpeech() {}
+                        override fun onError(i: Int) {
+                            Log.e("onError", i.toString())
+                        }
+
+                        override fun onResults(bundle: Bundle) {
+                            val data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                            text = data!![0]
+                        }
+
+                        override fun onPartialResults(bundle: Bundle) {}
+                        override fun onEvent(i: Int, bundle: Bundle) {}
+                    })
+                },
                 Modifier
                     .fillMaxWidth()
-                    .height(50.dp)
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(R.color.green),
+                    contentColor = White)
             ) {
                 Text(text = "Start")
             }
             Spacer(modifier = Modifier.height(10.dp))
             TextField(
-                value = space,
-                onValueChange = { space = it },
+                value = text,
+                onValueChange = { text = it },
                 Modifier
                     .fillMaxWidth()
-                    .height(50.dp)
+                    .height(50.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = White,
+                    textColor = colorResource(R.color.green)
+                )
             )
+            Spacer(modifier = Modifier.height(10.dp))
+            GoogleMap(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                cameraPositionState = cameraPositionState
+            ) {
+                Marker(
+                    state = MarkerState(position = seoul),
+                    title = "Singapore",
+                    snippet = "Marker in Singapore"
+                )
+            }
         }
     }
 
@@ -77,36 +138,5 @@ class MainActivity : AppCompatActivity() {
                 1
             )
         }
-    }
-
-    private fun startSpeech() {
-        var speechRecognizer: SpeechRecognizer? = null
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
-        speechRecognizer.setRecognitionListener(object : RecognitionListener {
-            override fun onReadyForSpeech(bundle: Bundle) {}
-            override fun onBeginningOfSpeech() {
-//                editText.setText("")
-//                editText.setHint("Listening...")
-            }
-
-            override fun onRmsChanged(v: Float) {}
-            override fun onBufferReceived(bytes: ByteArray) {}
-            override fun onEndOfSpeech() {}
-            override fun onError(i: Int) {}
-            override fun onResults(bundle: Bundle) {
-                val data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                Log.e("MainActivity", data!![0])
-//                micButton.setImageResource(R.drawable.ic_mic_black_off)
-//                val data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-//                editText.setText(data!![0])
-            }
-
-            override fun onPartialResults(bundle: Bundle) {}
-            override fun onEvent(i: Int, bundle: Bundle) {}
-        })
     }
 }
